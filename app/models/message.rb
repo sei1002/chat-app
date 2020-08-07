8841e88873d6a08ecc0316318b9a9f1f7f1233fc
sei1002 <sei1002210@gmail.com>
2020-08-07 14:17:41 +0900

    Active Storageの導入

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,13 @@
 class Message < ApplicationRecord
   belongs_to :room
   belongs_to :user
+  has_one_attached :image
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+  end
 
   validates :content, presence: true
 end
